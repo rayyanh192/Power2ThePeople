6272e63be8f0ce6c3d1966aec8c60448ae255036
Rayyan Hussain <rayyanh192@gmail.com>
2026-01-25 03:43:56 -0800

    Initial Commit

---
+++ Power2ThePeople/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Power2ThePeople
+//
+//  Created by Rayyan Hussain on 1/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
